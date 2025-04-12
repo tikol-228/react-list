c486c2f866e876be3b1e4af703f5fa625dff2195
Tim K <timk@Tims-MacBook-Air.local>
2025-04-12 16:28:20 +0300

    Initial commit

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-react-todo-app/',
+  base: 'https://tikol-228.github.io/react-list',
 })
